7265107293b3b16a64151e7681095b651ef48321
rdunder <r.dunder@gmail.com>
2025-05-29 14:08:19 +0200

    lock down create event endpoint

--- WebApi/Controllers/EventsController.cs
+++ WebApi/Controllers/EventsController.cs
@@ -5,41 +5,41 @@ using Service.Dtos;
 using Service.Interfaces;
 using Service.Models;
 
-namespace WebApi.Controllers
+namespace WebApi.Controllers;
+
+[ApiController]
+public class EventsController(IEventService service, ILogger<EventsController> logger) : ControllerBase
 {
-    [ApiController]
-    public class EventsController(IEventService service, ILogger<EventsController> logger) : ControllerBase
+    private readonly IEventService _service = service;
+    private readonly ILogger<EventsController> _logger = logger;
+
+    [HttpGet("/")]
+    public async Task<IActionResult> GetEvents()
     {
-        private readonly IEventService _service = service;
-        private readonly ILogger<EventsController> _logger = logger;
+        var events = await _service.GetAllAsync();
+        return events.Success 
+            ? Ok(events) 
+            : NotFound(events);
+    }
 
-        [HttpGet("/")]
-        public async Task<IActionResult> GetEvents()
-        {
-            var events = await _service.GetAllAsync();
-            return events.Success 
-                ? Ok(events) 
-                : NotFound(events);
-        }
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetEvent(Guid id)
+    {
+        _logger.LogInformation($"Incoming ID: {id}");
+        
+        var eventResult = await _service.GetByIdAsync(id);
+        return eventResult.Success 
+            ? Ok(eventResult) 
+            : NotFound(eventResult);
+    }
 
-        [HttpGet("{id:guid}")]
-        public async Task<IActionResult> GetEvent(Guid id)
-        {
-            _logger.LogInformation($"Incoming ID: {id}");
-            
-            var eventResult = await _service.GetByIdAsync(id);
-            return eventResult.Success 
-                ? Ok(eventResult) 
-                : NotFound(eventResult);
-        }
-
-        [HttpPost("/")]
-        public async Task<IActionResult> CreateEvent(EventDto dto)
-        {
-            var result = await _service.CreateAsync(dto);
-            return result.Success
-                ? Ok(result)
-                : BadRequest(result);
-        }
+    [Authorize]
+    [HttpPost("/")]
+    public async Task<IActionResult> CreateEvent(EventDto dto)
+    {
+        var result = await _service.CreateAsync(dto);
+        return result.Success
+            ? Ok(result)
+            : BadRequest(result);
     }
 }
